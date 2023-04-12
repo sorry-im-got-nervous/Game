e43a28edc98c72b962ca3db78a0531663e059094
sorry-im-got-nervous <117665318+sorry-im-got-nervous@users.noreply.github.com>
2023-04-13 00:15:58 +0800

    Delete Laba directory

--- Laba/texture.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEXTURE_H_INCLUDED
-#define TEXTURE_H_INCLUDED
-
-typedef void (*TTexProc) (unsigned char *data, int width, int height, int cnt);
-
-void Texture_LoadTexture(char *file_name, int *textureId, int wrap, int filter, TTexProc proc);
-
-#endif // TEXTURE_H_INCLUDED
-
